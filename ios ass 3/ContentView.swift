b606840a514e97a0d4cfcbcb505011eb1b127d75
Nikhil Jammula <nikhiljammula@Sreenikhils-MacBook-Pro.local>
2020-02-25 18:18:59 -0500

    Adding code

---
+++ ios ass 3/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ios ass 3
+//
+//  Created by Nikhil Jammula on 2/25/20.
+//  Copyright © 2020 Nikhil Jammula. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
