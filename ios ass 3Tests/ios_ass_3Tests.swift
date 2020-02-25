2f3b816c20c240dd1bc9c30f05b0924cd35fb9e0
Nikhil Jammula <nikhiljammula@Sreenikhils-MacBook-Pro.local>
2020-02-25 17:55:12 -0500

    Whole code first commit

---
+++ ios ass 3Tests/ios_ass_3Tests.swift
@@ -0,0 +1,34 @@
+//
+//  ios_ass_3Tests.swift
+//  ios ass 3Tests
+//
+//  Created by Nikhil Jammula on 2/25/20.
+//  Copyright © 2020 Nikhil Jammula. All rights reserved.
+//
+
+import XCTest
+@testable import ios_ass_3
+
+class ios_ass_3Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
